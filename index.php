23023a6b77c7fa18f25832ca4f027b9593afb70e
lclayton1977 <31969725+lclayton1977@users.noreply.github.com>
2023-09-21 18:30:25 +0300

    Update index.php
    adding new line

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <body bgcolor='white'>
 <pre>
 <?php
-print("<b><font color=\"blue\">!! Hello !! This is LaDerick Clayton,\n\n! Welcome ! to My Web Server Demo for CyberArk created with Ansble Thanks for visiting</font>\n\n");
+print("<b><font color=\"blue\">!! Hello !! This is LaDerick Clayton,\n\n! Welcome ! to My Web Server Demo for CyberArk created with Ansble \n\n Thanks for visiting</font>\n\n");
 print (`/usr/sbin/ifconfig`);
 ?>
 </pre>
