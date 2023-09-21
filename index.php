e95b6c001c44e79594cc952cd922d7a652b38332
lclayton1977 <31969725+lclayton1977@users.noreply.github.com>
2023-09-21 18:26:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <body bgcolor='white'>
 <pre>
 <?php
-print("<b><font color=\"blue\">!! Hello !! This is Shashi Kant,\n\n! Welcome ! to My Web Server</font>\n\n");
+print("<b><font color=\"blue\">!! Hello !! This is LaDerick Clayton,\n\n! Welcome ! to My Web Server Demo for CyberArk</font>\n\n");
 print (`/usr/sbin/ifconfig`);
 ?>
 </pre>
